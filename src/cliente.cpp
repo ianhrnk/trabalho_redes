441b24e5c6f0d515b4a1caf21d3caf63d7d3b8f1
Ian Haranaka <ianhrnk@gmail.com>
2020-12-11 10:45:36 -0400

    classes criadas

--- src/cliente.cpp
+++ src/cliente.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <map>
+#include <string>
+
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/socket.h>
@@ -6,35 +9,103 @@
 
 #define PORT 8080
 
+enum Command {
+  make_dir, remove_dir, ls, send_file, remove_file
+};
+
 class TCP_Client {
+ private:
+  int client_fd;
+  struct sockaddr_in server_addr;
 
+  void SetServerAddr()
+  {
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(PORT);
+  }
 
+ public:
+  TCP_Client()
+  {
+    client_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (client_fd < 0)
+    {
+      perror("Error in socket");
+      exit(EXIT_FAILURE);
+    }
+    std::cout << "Client socket created successfully\n";
+    SetServerAddr();
+  }
 
+  ~TCP_Client()
+  {
+    close(client_fd);
+  }
+
+  void Connect()
+  {
+    if (connect(client_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0)
+    {
+      perror("Error in connection");
+      exit(EXIT_FAILURE);
+    }
+    std::cout << "Connected to Server\n";
+  }
 };
 
 int main()
 {
-  int client_fd = socket(AF_INET, SOCK_STREAM, 0);
-  struct sockaddr_in server_addr;
+  Command command;
+  std::string _command;
+  std::map<std::string, Command> str_to_command = {
+    {"make_dir", make_dir},
+    {"remove_dir", remove_dir},
+    {"ls", ls},
+    {"send_file", send_file},
+    {"remove_file", remove_file}
+  };
 
-  if (client_fd < 0)
+  TCP_Client client;
+  client.Connect();
+
+  bool exit = false;
+  std::string in, name;
+  while (!exit)
   {
-    perror("Error in socket");
-    exit(EXIT_FAILURE);
+    std::getline(std::cin, in);
+    std::stringstream ss(in);
+    ss >> _command;
+    command = str_to_command[_command];
+
+    switch(command)
+    {
+      case make_dir:
+        ss >> name;
+        break;
+
+      case remove_dir:
+        ss >> name;
+        break;
+
+      case ls:
+        break;
+
+      case send_file:
+        break;
+
+      case remove_file:
+        ss >> name;
+        break;
+
+      case exit:
+        exit = true;
+        break;
+
+      default:
+        break;
+    }
   }
 
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_addr.s_addr = INADDR_ANY;
-  server_addr.sin_port = htons(PORT);
-
-  if (connect(client_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0)
-  {
-    perror("Error in socket");
-    exit(EXIT_FAILURE);
-  }
-
-  std::cout << "Connected to Server\n";
-
-  close(client_fd);
   return 0;
 }
--- src/servidor.cpp
+++ src/servidor.cpp
@@ -12,57 +12,83 @@
 
 class TCP_Server {
  private:
+  int server_fd, client_fd;
+  struct sockaddr_in server_addr, client_addr;
+  int optval = 1;
 
+  void SetAddr()
+  {
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(PORT);
+  }
 
+  void SetSockOpt()
+  {
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval)))
+    {
+      perror("Error in setsockopt");
+      exit(EXIT_FAILURE);
+    }
+  }
 
  public:
   TCP_Server()
   {
-
+    server_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (server_fd < 0)
+    {
+      perror("Error in socket");
+      exit(EXIT_FAILURE);
+    }
+    std::cout << "Server socket created successfully\n";
+    SetSockOpt();
+    SetAddr();
   }
 
   ~TCP_Server()
   {
-
+    close(server_fd);
+    close(client_fd);
   }
 
+  void Bind()
+  {
+    if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0)
+    {
+      perror("Error in bind");
+      exit(EXIT_FAILURE);
+    }
+    std::cout << "Binding successfull\n";
+  }
+
+  void Listen()
+  {
+    if (listen(server_fd, 3))
+      std::cout << "Listening...\n";
+  }
+
+  void Accept()
+  {
+    socklen_t addr_len = sizeof(client_addr);
+    client_fd = accept(server_fd, (struct sockaddr *) &client_addr, &addr_len);
+    if (client_fd < 0) exit(EXIT_FAILURE);
+    std::cout << "Connection established\n";
+  }
 };
 
 int main()
 {
-  int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-  struct sockaddr_in server_addr, client_addr;
-  socklen_t addr_len;
+  TCP_Server server;
+  server.Bind();
+  server.Listen();
+  server.Accept();
 
-  if (server_fd < 0)
+  bool exit = false;
+  while (!exit)
   {
-    perror("Error in socket");
-    exit(EXIT_FAILURE);
+
   }
 
-  std::cout << "Server socket created successfully\n";
-
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_addr.s_addr = INADDR_ANY;
-  server_addr.sin_port = htons(PORT);
-
-  if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0)
-  {
-    perror("Error in bind");
-    exit(EXIT_FAILURE);
-  }
-
-  std::cout << "Binding successfull\n";
-
-  if (listen(server_fd, 3))
-    std::cout << "Listening...\n";
-
-  addr_len = sizeof(client_addr);
-  int new_socket = accept(server_fd, (struct sockaddr *) &client_addr, &addr_len);
-
-  if (new_socket < 0) exit(EXIT_FAILURE);
-
-  close(server_fd);
-  close(new_socket);
   return 0;
 }
